1f15ea19f099efe3f8af1cfb2d91928f77ddc810
HEMANT KUMAR <hemantkumar08.dhn@gmail.com>
2024-06-29 11:22:27 +0530

    Initial changes

--- LinkList.java
+++ LinkList.java
@@ -15,6 +15,7 @@ public class LinkList {
             Node node1 = new Node(1);
             Node node2 = new Node(2);
             Node node3 = new Node(3);
+            Node node4 = new Node(4);
 
             // Linking nodes
             node1.next = node2;
